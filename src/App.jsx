79cff487497161f3f2226809b81a8f7f119840e7
sharada417 <sharada.kanchana2001@gmail.com>
2025-08-08 01:34:34 +0530

    Props simple example

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Simple from './Simple'
 
 function App() {
   
   return (
     <>
-     
+     <h1>This is simple example</h1>
+     <Simple name="Alice"/>
     </>
   )
 }
